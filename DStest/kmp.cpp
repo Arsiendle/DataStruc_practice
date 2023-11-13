34b193f52ad507a53f323541624431cb899bdd33
Arsiendle <arsiendle@zjxu.edu.cn>
2023-11-13 14:31:57 +0800

    增加nextval数组

--- DStest/kmp.cpp
+++ DStest/kmp.cpp
@@ -18,11 +18,10 @@ int *get_next(char *pattern)
     next[2] = 1;
 
     int i = 2, j = 0;
-    // next[i]记录着模式串中第i个字符前的最大前缀的长度,即最大前缀的下一位置的索引
-    // j记录着最大前缀的长度
+    // next[j] 的含义是 : 在子串的第j个字符与主串发生失配时, 则跳到子串的next[j] 位置重新与主串当前位置进行比较。
     while (i < pattern[0])
     {
-        if (j == 0 || pattern[i] == pattern[j])
+        if (j == 0 || pattern[i] == pattern[j]) // pattern[i]表示后缀的单个字符，pattern[j]表示前缀的单个字符
         {
             ++i;
             ++j;
@@ -30,16 +29,48 @@ int *get_next(char *pattern)
         }
         else
         {
-            j = next[j];
+            j = next[j]; // 若字符不相同，则j值回溯
         }
     }
     return next;
 }
 
+int *get_nextval(char *pattern)
+{
+    int *nextval = new int[pattern[0] + 1];
+    nextval[0] = pattern[0];
+    nextval[1] = 0;
+    nextval[2] = 1;
+
+    int i = 2, j = 0;
+    // nextval[j] 的含义是 : 在子串的第j个字符与主串发生失配时, 则跳到子串的nextval[j] 位置重新与主串当前位置进行比较。
+    while (i < pattern[0])
+    {
+        if (j == 0 || pattern[i] == pattern[j]) // pattern[i]表示后缀的单个字符，pattern[j]表示前缀的单个字符
+        {
+            ++i;
+            ++j;
+            if (pattern[i] != pattern[j])
+            {
+                nextval[i] = j;
+            }
+            else
+            {
+                nextval[i] = nextval[j];
+            }
+        }
+        else
+        {
+            j = nextval[j]; // 若字符不相同，则j值回溯
+        }
+    }
+    return nextval;
+}
+
 int Index_KMP(char *str, char *pattern)
 {
     int i = 1, j = 1;
-    int *next = get_next(pattern); // 得到next数组
+    int *next = get_nextval(pattern); // 得到next数组
 
     while (i <= str[0] && j <= pattern[0])
     {
